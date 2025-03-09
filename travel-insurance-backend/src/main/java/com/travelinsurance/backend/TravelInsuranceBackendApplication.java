ff7fe9689b29bec1adc4657a24382bdcfe3da6f5
jaimani1111 <113229601+jaimani1111@users.noreply.github.com>
2025-03-10 00:29:25 +0530

    Add travel-insurance-backend/src/main/java/com/travelinsurance/backend/TravelInsuranceBackendApplication.java 1741546764611

---
+++ travel-insurance-backend/src/main/java/com/travelinsurance/backend/TravelInsuranceBackendApplication.java
@@ -0,0 +1,11 @@
+package com.travelinsurance.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelInsuranceBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelInsuranceBackendApplication.class, args);
+    }
+}
